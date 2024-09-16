cd16876e8386833dda506ac2e74e97b8d37d77a0
Thibaut Chabrier <thibaut.chabrier0710@gmail.com>
2024-09-17 00:46:48 +0200

    Update CommandeService.cs remove httpclient

--- Service/CommandeService.cs
+++ Service/CommandeService.cs
@@ -1,48 +1,23 @@
-﻿using API_produit.Models;
-using System.Net.Http;
+using API_Commande.Context;
+using API_Commande.Models;
 using System.Text.Json;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace API_Commande.Service
 {
     public class CommandeService
     {
-        private readonly HttpClient _httpClient;
+        private readonly IRabbitMQService _rabbitMQService;
 
-        public CommandeService(HttpClient httpClient)
+        public CommandeService(IRabbitMQService rabbitMQService)
         {
-            _httpClient = httpClient;
+            _rabbitMQService = rabbitMQService;
         }
 
-        public async Task<List<Produit>> GetProduitsByIds(List<int> produitIds)
+        public async Task<List<Dictionary<string, object>>> GetProduitsByIds(List<int> produitIds)
         {
-            try
-            {
-                // Créer la chaîne de requête avec les IDs des produits
-                var produitIdsString = string.Join("&", produitIds.Select(id => $"produitsId={id}"));
-                var response = await _httpClient.GetAsync($"Produit/produitInCommande?{produitIdsString}");
-
-                response.EnsureSuccessStatusCode(); // Lève une exception si le code de statut est en erreur
-
-                var content = await response.Content.ReadAsStringAsync();
-                var produits = JsonSerializer.Deserialize<List<Produit>>(content, new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                    PropertyNameCaseInsensitive = true
-                });
-
-                return produits ?? new List<Produit>(); // Retourner une liste vide si la désérialisation échoue
-            }
-            catch (HttpRequestException httpEx)
-            {
-                throw new Exception("Erreur HTTP lors de la récupération des produits", httpEx);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Erreur lors de la récupération des produits", ex);
-            }
+            var response = await _rabbitMQService.GetProduitsByIds(produitIds);
+            return response;
         }
     }
+
 }
