e9503adc5ca11bf3fdccb1851f8476201136c307
maxime.bouzeghaia <91263301+ZylinderMan@users.noreply.github.com>
2024-09-17 19:22:24 +0200

    Secu + Nuggets

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 using API_Commande.Context;
 using API_Commande.Service;
 using ConsumerAPI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Configuration JWT pour l'authentification
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+// Configuration CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        policy => policy.WithOrigins("http://localhost:5239")  // URL de l'API Commande
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
 
 builder.Services.AddDbContext<CommandeContext>(options =>
     options.UseMySql(
@@ -20,7 +51,6 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<CommandeService>();
 builder.Services.AddSingleton<IRabbitMQService, RabbitMQService>();
 builder.Services.AddSingleton<RabbitMQConsumer>();
-builder.WebHost.UseUrls("http://0.0.0.0:5239");
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -36,6 +66,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//app.UseCors("CorsPolicy");
+
+// Activer l'authentification et l'autorisation
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
